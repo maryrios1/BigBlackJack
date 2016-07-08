66e311afa9d5da3b2dd10e873edfd2ef5eb29e13
kleer <ljgarza89@gmail.com>
2016-07-08 11:50:16 -0500

    clase Card

---
+++ spec/cartas_spec.rb
@@ -0,0 +1,14 @@
+require './lib/card.rb'
+
+describe Card do
+  it "se crea una carta con valor A" do
+    carta = Card.new 1,"Corazon"
+    carta.value.should == 11
+  end
+
+  it "se crea una carta con valor K de diamantes y regresa 10 de diamantes" do
+    carta = Card.new 13,"Diamante"
+    carta.value.should == 10
+  end
+
+end

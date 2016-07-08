6114ecfaa365216616cc194e910bd77de41a96a4
maryc_182@hotmail.com <maryc_182@hotmail.com>
2016-07-08 12:22:02 -0500

    dealer 2

--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -1,7 +1,7 @@
 require './lib/dealer'
 
 describe Dealer do
-  it "repartir dos cartas y evaluar que no se encuentre en la baraja" do
+  it "repartir una carta y evaluar que no se encuentre en la baraja" do
     #arrange
     dealer = Dealer.new
     #act
@@ -12,4 +12,20 @@ describe Dealer do
     listLength.should == 11
     exist.should == nil
   end
+
+  it "repartir dos cartas y evaluar que no se encuentre en la baraja" do
+    #arrange
+    dealer = Dealer.new
+    #act
+    carta1 = dealer.repartirCarta
+    carta2 = dealer.repartirCarta
+    listLength = dealer.listLength
+    exist1 = dealer.isCarta carta1
+    exist2 = dealer.isCarta carta2
+    #assert
+    listLength.should == 10
+    exist1.should == nil
+    exist2.should == nil
+  end
+  
 end
